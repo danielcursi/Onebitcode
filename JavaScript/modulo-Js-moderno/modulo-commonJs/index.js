4566b39f40abe6eae1c3d046fa78878b10bd0c5a
Daniel Cursi <105112847+danielcursi@users.noreply.github.com>
2024-10-14 13:01:28 -0300

    modulo commonJs

---
+++ JavaScript/modulo-Js-moderno/modulo-commonJs/index.js
@@ -0,0 +1,7 @@
+const render = require("./render")
+const store = require("./store")
+
+console.log("Iniciando a aplicação")
+render()
+store()
+console.log("Finalizando Sistema!")
---
+++ JavaScript/modulo-Js-moderno/modulo-commonJs/render.js
@@ -0,0 +1,5 @@
+function render(){
+    console.log("Iniciando a interface da aplicação!")
+}
+
+module.exports = render
