8e1fd90d71f7b7c18b8bc027e61322843259d7aa
Daniel Cursi <105112847+danielcursi@users.noreply.github.com>
2024-11-04 19:27:23 -0300

    .

---
+++ JavaScript/modulo-Js-moderno/operador-coalescencia-nula/index.js
@@ -0,0 +1,25 @@
+// const a = 0 // false
+
+// const b = null // false
+
+// const c = 'teste' // true
+
+// console.log(a || b || c)
+
+// const a = 0
+
+// const b = null
+
+// const c = 'teste'
+
+// console.log(a ?? b ?? c)
+
+const a = 0
+
+let b = a || 42
+
+console.log({a , b})
+
+b = a ?? 42
+
+console.log({a , b})
