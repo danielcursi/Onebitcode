dabaac725b4f17c51a438ad8fa90a642df244a1e
Daniel Cursi <105112847+danielcursi@users.noreply.github.com>
2024-12-09 20:15:56 -0300

    assincronicidade

---
+++ JavaScript/modulo-6/07-assincronicidade/index.js
@@ -0,0 +1,15 @@
+// Sincrono
+function step02(){
+    console.log('Passo 02')
+}
+
+console.log('Passo 01')
+step02()
+console.log('Passo 03')
+
+// Assincrono
+console.log('Passo 04')
+setTimeout(() => {
+    console.log('Passo 05')
+}, 1)
+console.log('Passo 06')
