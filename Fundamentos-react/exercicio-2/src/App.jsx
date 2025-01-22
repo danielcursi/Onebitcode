a7ffd82883b82751efd0cbb3f3ceb79c8364b242
Daniel Cursi <105112847+danielcursi@users.noreply.github.com>
2025-01-22 18:52:07 -0300

    Introducao do metodo Fragment

--- Fundamentos-react/exercicio-2/src/App.jsx
+++ Fundamentos-react/exercicio-2/src/App.jsx
@@ -1,7 +1,13 @@
+import { Fragment } from "react";
 import Card from "./Card";
 
-export default function App(){
+export default function App() {
   return (
-    <Card />
+    // Fragment usado para ter mais de um componente em precisar usar uma div pai
+    // pode ser abreviado com <></>
+    <Fragment>
+      <h1>Teste</h1>
+      <Card />
+    </Fragment>
   )
 }
