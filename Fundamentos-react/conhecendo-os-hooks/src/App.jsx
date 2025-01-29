ff4005e4f585a402c2f9205b471afbb5a9d5c42a
Daniel Cursi <105112847+danielcursi@users.noreply.github.com>
2025-01-29 16:55:31 -0300

    exercicio

---
+++ Fundamentos-react/conhecendo-os-hooks/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import useCounter from './hooks/useCounter'
+
+function App() {
+  const counter = useCounter()
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={counter.increment}>
+          count is {counter.count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
