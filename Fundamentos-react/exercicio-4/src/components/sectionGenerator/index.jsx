e94c162547eb24acd59b6b7637e0bc40c864c261
Daniel Cursi <105112847+danielcursi@users.noreply.github.com>
2025-01-31 15:20:06 -0300

    atividade concluida, botao de copiar funcionando!

--- Fundamentos-react/exercicio-4/src/components/sectionGenerator/index.jsx
+++ Fundamentos-react/exercicio-4/src/components/sectionGenerator/index.jsx
@@ -7,7 +7,12 @@ import getPassword from "../geradorSenhas.js"
 export default function sectionGenerator() {
     const [textCopied, setTextCopied] = useState('Copiar!')
     function copied() {
-        setTextCopied('Copiado!')
+        if(!password)
+            return alert("Nada a ser copiado!")
+        
+        navigator.clipboard.writeText(password).then(() => {
+            setTextCopied('Copiado!')
+        })
     }
 
     const [password, setPassword] = useState('')
